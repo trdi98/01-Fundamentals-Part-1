64bea26db65733bf1ebf1d3611b65967e15b737a
trdi98 <166541956+trdi98@users.noreply.github.com>
2024-10-26 18:17:04 +0200

    first commit

---
+++ starter/assignments.js
@@ -0,0 +1,95 @@
+/*
+//Values and variables
+
+let country = "Croatia";
+let continent = "Europe";
+let population = 4;
+let language = "Croatian";
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+
+//Data types
+
+let isIsland = false;
+
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+//console.log(typeof language);
+
+
+/* let var and const
+
+ language = "Croatian";
+
+const continent = "Europe";
+const country = "Croatia";
+const isIsland = false;
+//isIsland = true;
+
+
+//console.log(language);
+console.log(continent);
+console.log(country);
+console.log(isIsland);
+
+
+// Basic operators
+
+console.log(population / 2);
+
+population ++;
+console.log(population);
+
+let finland = 6000000;
+console.log(finland < population);
+
+console.log(population < 33000000);
+
+
+country + 
+' is in ' + 
+continent + 
+' and its ' +
+population +
+' speak ' +
+language;
+
+console.log(description);
+
+
+// Strings and template literals
+
+let description = `${country} is in ${continent}, and its ${population} speak ${language}`;
+console.log(description);
+
+//Taking decisions: if/else Statements
+
+if(population > 33) {
+    console.log(`${country}'s population is above average` );
+} else {
+    console.log(`${country}'s population is ${33 - population} milion below average`);
+}
+ 
+console.log('9' - '5'); // -> '4'
+console.log('19' - '13' + '17'); // -> '617'
+console.log('19' - '13' + 17); // -> 23
+console.log('123' < 57); // -> false
+console.log(5 + 6 + '4' + 9 - 4 - 2); // -> 1143
+
+
+
+// TYPE CONVERSION AND COERCION 
+
+console.log('9' - '5'); // -> 4
+console.log('19' - '13' + '17'); // -> 617
+console.log('19' - '13' + 17); // -> 23
+console.log('123' < 57); // -> false
+console.log(5 + 6 + '4' + 9 - 4 - 2); // -> 1143
+
+*/
+
---
+++ starter/challenges.js
@@ -0,0 +1,69 @@
+/*
+let massMark = 78;
+let heightMark = 1.69;
+let massJohn = 92;
+let heightJohn = 1.95;
+
+let BMIMark = massMark / (heightMark * heightMark);
+console.log(BMIMark);
+
+let BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIJohn);
+
+let markHigherBMI = BMIMark > BMIJohn;
+console.log(markHigherBMI);
+
+massMark = 95;
+heightMark = 1.88;
+massJohn = 85;
+heightJohn = 1.76;
+
+
+BMIMark = massMark / (heightMark * heightMark);
+BMIJohn = massJohn / (heightJohn * heightJohn);
+markHigherBMI = BMIMark > BMIJohn;
+console.log(BMIMark, BMIJohn);
+
+if (BMIMark > BMIJohn) {
+    console.log(`Mark's BMI (${BMIMark}) is higher then John's (${BMIJohn})!`)
+} else {
+    console.log(`John's BMI (${BMIJohn}) is higher then Mark's (${BMIMark}) !`)
+}
+    
+
+const scoreDolphins = (96 + 108+ 89) / 3;
+const scoreKoalas = (88 + 91+ 110) / 3;
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas) {
+    console.log('Dolphins win the trophy!');
+} else if (scoreKoalas > scoreDolphins) {
+    console.log('Koalas win the trophy!');
+} else if (scoreDolphins === scoreKoalas) {
+    console.log('Both win the trophy!')
+}
+
+
+
+const scoreDolphins = (97 + 112+ 80) / 3;
+const scoreKoalas = (109 + 95+ 50) / 3;
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+    console.log('Dolphins win the trophy!');
+} else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
+    console.log('Koalas win the trophy!');
+} else if (scoreDolphins === scoreKoalas && scoreDolphins >= 100 && scoreKoalas >= 100) {
+    console.log('Both win the trophy!')
+} else {
+    console.log('No one wins the trophy');
+}
+
+
+
+const bill = 430;
+const tip = bill <= 300 && bill >= 50  ? bill * 0.15 : bill * 0.2
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+
+*/
